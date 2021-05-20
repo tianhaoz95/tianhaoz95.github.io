b84eb390088c038e04a35c6e39a4d788f404a4e9
Tianhao Zhou <tianhaoz@umich.edu>
2021-05-20 16:05:52 -0700

    fix: edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,13 +85,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/tianhaoz95/tianhaoz95.github.io/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/tianhaoz95/tianhaoz95.github.io/edit/main/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
