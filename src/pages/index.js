a30aef2434329e74be2318354eb77daef8edd8c2
Tianhao Zhou <tianhaoz@umich.edu>
2021-05-20 11:41:40 -0700

    fix: broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs">
+            to="/docs/about_me">
             About Me
           </Link>
         </div>
